4f5a5023e4afbee31108e67b6a84053ef71a139c
Jacob Gersztyn <jabocg@gmail.com>
2017-09-01 12:24:01 -0700

    Add .gitignore, update igpay to be library
    No longer has a main, has to be imported to use. Also fix a bug with the
    regular string method.

--- igpayatinlay.py
+++ igpayatinlay.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3.6
 """Translate things to Pig Latin or something."""
-import sys
 
 
-VOWELS = 'a e i o u'.split()
-CONSONANTS = 'b c d f g h j k l m n p q r s t v w x y z'.split()
-CAPITALS = 'B C D F G H J K L M N P Q R S T Y V W X Y Z'.split()
-PUNCTUATION_MARKS = ', . : ! " ?'.split()
+_VOWELS = 'a e i o u'.split()
+_CONSONANTS = 'b c d f g h j k l m n p q r s t v w x y z'.split()
+_CAPITALS = 'B C D F G H J K L M N P Q R S T Y V W X Y Z'.split()
+_PUNCTUATION_MARKS = ', . : ! " ?'.split()
 
 
-def split_into_parts(string):
+def _split_into_parts(string):
     first_vowel = -1
     punctuation = -1
     for i, c in enumerate(string):
-        if c in VOWELS and first_vowel == -1:
+        if c in _VOWELS and first_vowel == -1:
             first_vowel = i
-        elif c in PUNCTUATION_MARKS and punctuation == -1:
+        elif c in _PUNCTUATION_MARKS and punctuation == -1:
             punctuation = i
             break
     if first_vowel == -1:
@@ -36,6 +35,7 @@ class IgpayAtinlayAsephray:
     def __init__(self, phrase=None):
         """Construct an IgpayAtinlay object.  """
         self.pig_phrase = None
+        self.pig_words = []
         if phrase is None:
             self.words = []
         elif type(phrase) is list:
@@ -63,13 +63,13 @@ class IgpayAtinlayAsephray:
         for i, word in enumerate(self.words):
             split_words = word.split()
             for j, v in enumerate(split_words):
-                pre, rest, punc = split_into_parts(v)
-                if len(pre) > 0 and pre[0] in CAPITALS:
+                pre, rest, punc = _split_into_parts(v)
+                if len(pre) > 0 and pre[0] in _CAPITALS:
                     pre = pre.lower()
                     rest = rest[0].upper() + rest[1:]
                 split_words[j] = '{}{}ay{}'.format(rest, pre, punc)
-            self.words[i] = ' '.join(split_words)
-        self.pig_phrase = ' '.join(self.words)
+            self.pig_words.append(' '.join(split_words))
+        self.pig_phrase = ' '.join(self.pig_words)
 
     def __str__(self):
         """Return a string version of the Pig Latin phrase."""
@@ -77,9 +77,3 @@ class IgpayAtinlayAsephray:
             self._set_pig_phrase()
 
         return self.pig_phrase
-
-
-if __name__ == "__main__":
-    pig_latin_phrase = IgpayAtinlayAsephray(sys.argv[1:])
-    print(pig_latin_phrase.asay_ormalnay_ingstray())
-    print(pig_latin_phrase)
